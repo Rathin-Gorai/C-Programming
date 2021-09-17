24bbe1be313d7b97ea8942d14bcdcec3cf53049b
Rathin Kumar Gorai <89589036+Rathin-Gorai@users.noreply.github.com>
2021-09-17 20:54:04 +0530

    Check the number positive or negative
    C program to check whether a given number is positive or negative.

---
+++ 8.c
@@ -0,0 +1,22 @@
+/*3. Write a C program to check whether a given number is positive or negative.
+Test Data : 15
+Expected Output :
+15 is a positive number*/
+
+#include<stdio.h>
+int main()
+{
+    int a;
+	//input--------------------------
+    printf("Test Data: ");
+    scanf("%d", &a);
+	// condition and output-----------------
+    if 
+    ( a < 0 )
+    printf("%d is a negative number",a);
+    else
+    printf("%d is positive number", a);
+    
+}
+	//Author- Rathin Kumar Gorai
+	//Date-17/08/2021
