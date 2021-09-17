4486b1bb31a672f51fe57ec3f4a792f04f708e56
Rathin Kumar Gorai <89589036+Rathin-Gorai@users.noreply.github.com>
2021-09-17 19:19:03 +0530

    C program that converts Centigrade to Fahrenheit.
    Write a program that converts Centigrade to Fahrenheit.

---
+++ 1.c
@@ -0,0 +1,20 @@
+/*
+Write a program that converts Centigrade to Fahrenheit.
+Expected Output :
+Input a temperature (in Centigrade): 45
+113.000000 degrees Fahrenheit       
+*/
+
+#include<stdio.h>
+int main()
+{
+    float centigrade,fahrenheit;
+	//input------------------------------------------
+    printf(" Input a temperature (in Centigrade):  "); 
+    scanf("%f", &centigrade);
+	//formula-----------------------------------------
+    fahrenheit=(centigrade*9/5)+32; 
+	//output------------------------------------------
+    printf(" fahrenheit= %f", fahrenheit); 
+    return 0;
+}
