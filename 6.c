12f794b79a051185df79ac11804ae77dead6d496
Rathin Kumar Gorai <89589036+Rathin-Gorai@users.noreply.github.com>
2021-09-17 20:49:21 +0530

    Check two number equal or not
    C program to accept two integers and check whether they are equal or not.

---
+++ 6.c
@@ -0,0 +1,26 @@
+/*1. Write a C program to accept two integers and check whether they are equal or not.  
+Test Data : 15 15
+Expected Output :
+Number1 and Number2 are equal
+*/
+
+#include<stdio.h>
+int main()
+{
+	int number1, number2;
+	//input----------------------------
+    printf("Enter the First Number: ");
+    scanf("%d", &number1);
+    printf("Enter the Second Number: ");
+    scanf("%d", &number2);
+	//process(condition) and Output--------------
+    if
+    (number1==number2)
+    printf("The Number is Equal"); 
+    else
+    printf("The Number are not Equal");
+    return 0;
+	
+}	
+	//Author- Rathin Kumar Gorai
+	//Date-17/08/2021
