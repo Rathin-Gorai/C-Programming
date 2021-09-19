8b810e55357e36c0c61d6261fe09a2d25ae1905f
Rathin Kumar Gorai <89589036+Rathin-Gorai@users.noreply.github.com>
2021-09-19 14:41:40 +0530

    Check Vowel or consonant
    C program to check whether an alphabet is a vowel or consonant.

---
+++ 15.c
@@ -0,0 +1,47 @@
+/*5. Write a C program to check whether an alphabet is a vowel or consonant.  
+Test Data :
+k
+Expected Output :
+The alphabet is a consonant.*/
+
+#include<stdio.h>
+int main()
+{
+    char alphabet,a,e,i,o,u;
+	//input--------------------------
+    printf("Test Data= ");
+    scanf("%s", &alphabet);
+	//condtion and output-------------------------
+    if 
+    (alphabet == 'a')
+    {
+        printf(" The alphabet is vowel ", alphabet);
+    }
+    else if 
+    (alphabet == 'e')
+    {
+        printf(" The alphabet is vowel ", alphabet);
+    }
+    else if 
+    (alphabet == 'i')
+    {
+        printf(" The alphabet is vowel ", alphabet);
+    }
+    else if 
+    (alphabet == 'o')
+    {
+        printf(" The alphabet is vowel ", alphabet);
+    }
+    else if 
+    (alphabet = 'u')
+    {
+        printf(" The alphabet is vowel  ", alphabet);
+    }
+    else
+    {
+        printf(" The alphabet is consonent  ", alphabet);
+    }
+    return 0;
+}
+	//Author- Rathin Kumar Gorai
+	//Date-17/08/2021
